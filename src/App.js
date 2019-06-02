410462abe304dd81d20ea2805ce96024c519a33c
Miguel Machado <miiguellmp@gmail.com>
2019-06-02 18:15:51 -0300

    getting the search term from SearchBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import SearchBar from './components/SearchBar';
+import youtube from './apis/youtube';
 
 export default class App extends Component {
+  state = {
+    videos: [],
+  };
+
+  // getting the term from SearchBar component and using axios to
+  // get the results
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
-    return <div>App</div>;
+    const { videos } = this.state;
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have {videos.length} videos.
+      </div>
+    );
   }
 }
